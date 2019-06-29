6c9acc714c14f8af29bcb5fc0f171d9c767f569e
ROWENAR <50422275+ROWENAR@users.noreply.github.com>
2019-06-29 12:42:54 -0500

    Update chapter9_challenge3.py

--- part_I/files/challenges/chapter9_challenge3.py
+++ part_I/files/challenges/chapter9_challenge3.py
@@ -1,7 +1,7 @@
 import csv
 
 movies = [["Top Gun", "Risky Business", "Minority Report"], ["Titanic", "The Revenant", "Inception"], ["Training Day", "Man on Fire", "Flight"]]
-with open("movies.csv", "w") as csvfile:
+with open("movies.csv", "w", newline='') as csvfile: # newline parameter added in Python3 to allow for no blank lines in csv
     spamwriter = csv.writer(csvfile, delimiter=",")
     for movie_list in movies:
         spamwriter.writerow(movie_list)
